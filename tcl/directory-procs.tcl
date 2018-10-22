785a97f7b7ba9cea34402c797a5661deb68bb462
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-10-22 08:32:17 +0000

    - use ad_conn features available since 10+ years instead of local subsite operations - remove dependency on tree_sortkey on site_nodes

--- tcl/directory-procs.tcl
+++ tcl/directory-procs.tcl
@@ -94,30 +94,16 @@ ad_proc dir_alpha_nav_bar {
 
 ad_proc dir_app_group_info {} {
 
-    Return a list containing the group_id (lindex 0) and group_name (lindex 1) of the application group associated with the subsite that the current directory package is mounted under.  
-    The directory package does not need to be mounted directly under the subsite; it will find the closest instance of subsite than is an ancestor of the package.
+    Return a list containing the group_id (lindex 0) and group_name
+    (lindex 1) of the application group associated with the subsite
+    that the current directory package is mounted under.  The
+    directory package does not need to be mounted directly under the
+    subsite; it will find the closest instance of subsite than is an
+    ancestor of the package.
 
 } {
     
-    set package_id [ad_conn package_id]
-    db_1row n_id {
-        select node_id from site_nodes
-        where object_id = :package_id
-    }
-    
-    # find the closest subsite to the current package on the site_nodes tree
-    # the inner query is guaranteed to always return at least 1 row, 
-    # because the Main Site subsite is mounted at /
-    db_1row subsite_p_1 {
-        select * from (
-          select object_id as subsite_id
-            from site_nodes n
-           where (select package_key from apm_packages p where p.package_id = n.object_id) = 'acs-subsite' 
-         connect by node_id = prior parent_id 
-           start with node_id = :node_id 
-           order by level
-        ) where rownum = 1
-    }
+    set subsite_id [ad_conn subsite_id]
     
     db_1row g_i {
       select g.group_id, g.group_name
@@ -131,30 +117,15 @@ ad_proc dir_app_group_info {} {
 
 ad_proc dir_subsite_info {} {
 
-    Return a list containing the subsite_id (lindex 0) and subsite_name (lindex 1) of the subsite that the current directory package is mounted under.  
-    The directory package does not need to be mounted directly under the subsite; it will find the closest instance of subsite than is an ancestor of the package.
+    Return a list containing the subsite_id (lindex 0) and
+    subsite_name (lindex 1) of the subsite that the current directory
+    package is mounted under.  The directory package does not need to
+    be mounted directly under the subsite; it will find the closest
+    instance of subsite than is an ancestor of the package.
 
 } {
-    
-    set package_id [ad_conn package_id]
-    db_1row n_id {
-        select node_id from site_nodes
-        where object_id = :package_id
-    }
-    
-    # find the closest subsite to the current package on the site_nodes tree
-    # the inner query is guaranteed to always return at least 1 row, 
-    # because the Main Site subsite is mounted at /
-    db_1row subsite_p_2 {
-        select * from (
-          select object_id as subsite_id, acs_object.name(object_id) as subsite_name
-            from site_nodes n
-           where (select package_key from apm_packages p where p.package_id = n.object_id) = 'acs-subsite' 
-         connect by node_id = prior parent_id 
-           start with node_id = :node_id 
-           order by level
-        ) where rownum = 1
-    }
+    set subsite_id [ad_conn subsite_id]
+    set subsite_name [subsite::get_element -subsite_id $subsite_id -element $instance_name]
     
     return [list $subsite_id $subsite_name]
 }
